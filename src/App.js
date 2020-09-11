a77fb13b0e91b93edb8073f807f1a6c855c8a93e
Yogeswarreddy Vennapusa <yogeswarreddy19942@gmail.com>
2020-09-11 16:13:26 -0400

    changed text

--- src/App.js
+++ src/App.js
@@ -10,14 +10,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p
+          className="App-intro">
+          I was changed on the feature branch
+        </p>
       </header>
     </div>
   );
